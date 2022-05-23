35b818f0655164508ed3fd5198bae20b5d455bb2
fx_kirin <fx.kirin@gmail.com>
2022-05-23 18:59:54 +0900

    fixed

--- kaniattrdict/__init__.py
+++ kaniattrdict/__init__.py
@@ -2,9 +2,9 @@
 attrdict contains several mapping objects that allow access to their
 keys as attributes.
 """
-from attrdict.mapping import AttrMap
-from attrdict.dictionary import AttrDict
-from attrdict.default import AttrDefault
+from .mapping import AttrMap
+from .dictionary import AttrDict
+from .default import AttrDefault
 
 
 __all__ = ['AttrMap', 'AttrDict', 'AttrDefault']
